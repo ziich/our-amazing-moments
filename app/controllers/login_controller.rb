f8f24d501235a5b52c2e9ff9da2a6274d1cf1640
Fabio Pinto <fabio.aa.pinto@hotmail.com>
2018-08-06 17:31:30 +0800

    init rails api

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,34 @@
+class LoginController < ApplicationController
+
+  URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
+
+
+def login
+
+    @user = User.find_or_create_by(open_id: wechat_user.fetch("openid"))
+
+    render json: {
+      userId: @user.id
+    }
+
+end
+
+
+def wechat_params
+
+    { appid: appId,
+    secret: appSecret,
+    js_code: params[:code],
+    grant_type: "authorization_code" }
+
+  end
+
+def wechat_user
+
+    @wechat_response ||= RestClient.post( URL, wechat_params )
+
+    @wechat_user ||= JSON.parse(@wechat_response.body)
+
+  end
+
+end
