29f6d80f26304f0e9f139fc7ce3479ef1be22520
yunshengji <423450928@qq.com>
2018-08-07 19:17:33 +0800

    now the user could have show, index,create method

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -8,9 +8,34 @@ class Api::V1::PostsController < Api::V1::BaseController
   def show
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @post.destroy
+    head :no_content
+  end
+
+
+
   private
 
   def set_post
     @post = Post.find(params[:id])
   end
+
+  def post_params
+    params.require(:post).permit(:content, :date, :user )
+  end
+
+  def render_error
+    render json: { errors: @story.errors.full_messages }, status: :unprocessable_entity
+  end
+
 end
