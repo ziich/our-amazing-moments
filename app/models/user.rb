c9962cc3b9ed28248c1bb0243bb5e4a21a75a22d
Christopher Chen <chenchristophert@gmail.com>
2018-08-10 17:23:09 +0800

    please god help us fix this fuckup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   acts_as_voter
   has_many :comments
   has_many :posts
-  validates :name, presence: true
-  validates :avatar, presence: true
+  # validates :name, presence: true
+  # validates :avatar, presence: true
 end
